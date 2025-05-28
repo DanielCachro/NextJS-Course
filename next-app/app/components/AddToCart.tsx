8c7920c4540cb5d40083339dadf4aff6d9ca67e9
DanielCachro <danielcachro@gmail.com>
2025-05-28 18:32:11 +0200

    Lessons: Next.js Fundamentals (11/11) - Setting Up Environment; Project Structure; Routing and Navigation; Client and Server Components; Data Fetching; Caching; Static and Dynamic Rendering.

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function AddToCart() {
+	return (
+		<div>
+			<button onClick={() => console.log('Click')}>Add to Cart</button>
+		</div>
+	)
+}
---
+++ next-app/app/users/page.tsx
@@ -0,0 +1,21 @@
+interface User {
+	id: number
+	name: string
+}
+
+export default async function UsersPage() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
+	const users: User[] = await res.json()
+
+	return (
+		<>
+			<h1>Users</h1>
+			<p>{new Date().toLocaleTimeString()}</p>
+			<ul>
+				{users.map(user => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+		</>
+	)
+}
